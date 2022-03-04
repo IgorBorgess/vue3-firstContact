842087fe868c25e6895bca29c4472536954c8f6d
Igor Borges <igorborgeswrk@gmail.com>
2022-03-03 22:49:55 -0300

    [If applied] will build nothing but the first stages of learning Vue3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FirstComponent />
+  <p>The test is being: {{ testing }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirstComponent from "./components/FirstComponent.vue"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    FirstComponent,
+    },
+  data() {
+    return {
+      testing: "made"
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Hello, Vue!</h1>
+        <p>My name is {{ name }}, I'm working as a {{ profession }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FirstComponent",
+    data() {
+        return {
+            name: "Igor",
+            profession: "Dev"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
