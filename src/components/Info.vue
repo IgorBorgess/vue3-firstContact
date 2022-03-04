c5a25a7e81bf5cebafb6fdea7dc23439e048ffed
Igor Borges <igorborgeswrk@gmail.com>
2022-03-04 00:55:50 -0300

    [If applied] will upload the directive update to Info.vue

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <h3>I'm working at the moment.</h3>
+        <h3 v-if="is_employed">I'm working at the moment.</h3>
+        <h3 v-else>I'm unemployed at the moment.</h3>
         <h3>I'm accostumed to using the following technologies:</h3>
         <u>
             <li>HTML</li>
@@ -9,12 +10,20 @@
             <li>Vue.js</li>
             <li>TailwindCSS</li>
         </u>
+        <h3 v-show="show_email">Send a message to {{ email }}</h3>
     </div>
 </template>
 
 <script>
 export default {
-
+    name: "Info",
+    data() {
+        return {
+            is_employed: false,
+            show_email: true,
+            email: "testingemail@email.com"
+        }
+    }
 }
 </script>
 
