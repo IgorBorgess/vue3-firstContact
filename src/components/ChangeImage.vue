80aaaf1279a565994c696792831ba3627b82a43e
Igor Borges <igorborgeswrk@gmail.com>
2022-03-15 01:43:19 -0300

    [If applied] will build the changeImage component while applying it to Picture.vue

---
+++ src/components/ChangeImage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button @click="$emit('changeImage')">Change profile image</button>
+        <button @click="$emit('revertChange')">Revert profile image</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ChangeImage",
+    emits: ["changeImage", "revertChange"]
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,23 +1,34 @@
 <template>
     <img v-bind:src="avatar" :alt="description">
-    <Reuse />
+    <ChangeImage @change-image="changeImage" @revert-change="revertChange" />
 </template>
 
 <script>
-import Reuse from "./Reuse.vue"
+import ChangeImage from "./ChangeImage.vue"
 
 export default {
     name: "Picture",
     components: {
-        Reuse
+        ChangeImage
     },
     data() {
         return {
             avatar: "/img/avatar.png",
             description: "Guy with a computer"
         }
+    },
+    methods: {
+        changeImage() {
+            this.avatar = "/img/avatar2.png"
+            console.log(this.avatar)
+        },
+        revertChange() {
+            this.avatar = "/img/avatar.png"
+            console.log(this.avatar)
+        }
     }
 }
+
 </script>
 
 <style>
